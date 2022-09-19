07f8c686a8ea0bf6fcb5d22a3ab360dfaf6e4282
quyeticb <quyeticb@gmail.com>
2022-09-19 11:09:56 +0700

    Add files via upload

---
+++ config.inc.php
@@ -0,0 +1,14 @@
+<?php
+	$cfg['blowfish_secret'] = '8ca9bff9ba4edee4bfcb654c647267b3';
+
+	$i = 0;
+
+	$i++;
+	$cfg['Servers'][$i]['auth_type'] = 'cookie';
+	$cfg['Servers'][$i]['host'] = '127.0.0.1';
+	$cfg['Servers'][$i]['compress'] = false;
+	$cfg['Servers'][$i]['AllowNoPassword'] = false;
+
+	$cfg['UploadDir'] = '';
+	$cfg['SaveDir'] = '';
+
